5f01dee94d9bb078c10694fb7b0aaa760d12ec49
Karl Söderbäck <karso527@student.liu.se>
2020-04-13 13:12:27 +0200

    NEW: Setup databasehelper and restructured server to init db connection

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,4 +1,20 @@
-var http = require('http');
+
+const dbHelper = require('../db/dbHelper.js')
+const express = require('express');
+const app = express()
+const port = 8080
+
+dbHelper.databaseConnect((err)=>{
+    if(err) console.log(err)
+});
+
+//TODO - add routes and register them with app like this:
+//routes(app);
+
+app.listen(port, () => console.log('Server running on port: ' + port))
+
+
+/*var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -6,3 +22,4 @@ http.createServer(function (req, res) {
 }).listen(8080)
 
 console.log('Server running on localhost:8080')
+*/
