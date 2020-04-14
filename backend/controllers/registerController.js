db87ec2b95a3511ce1ead6ef11cb2fbc0a8b201c
Karl Söderbäck <karso527@student.liu.se>
2020-04-14 14:03:05 +0200

    NEW: Added controller for registering new users

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -1,6 +1,11 @@
 const dbHelper = require('../db/dbHelper.js')
+const response = require('../modules/response.js')
 
 
 exports.post = async function(req, res) {
-    
+    if(dbHelper.createUser(req.body.name, req.body.password)) {
+        response.createSuccessResponse(res);
+    } else {
+        response.createFailResponse(res);
+    }
 }
