ae5d7d8b2e35dcd4054145c49e9564f23a393ae4
Karl Söderbäck <karso527@student.liu.se>
2020-04-23 09:06:52 +0200

    NEW: Added java server based on javalin

---
+++ backend/src/se/cinellections/CinecollectionsServer.java
@@ -0,0 +1,11 @@
+package se.cinellections;
+
+import se.cinellections.Server.Server;
+
+public class CinecollectionsServer {
+
+    public static void main(String[] args) {
+	    Server server = new Server();
+	    Server.run();
+    }
+}
