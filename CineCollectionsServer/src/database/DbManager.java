f1b99de883fffdda01fcce7abdbc027f34cf56dd
Karl Söderbäck <karso527@student.liu.se>
2020-04-24 13:10:14 +0200

    NEW: Set up necessary database tables

--- CineCollectionsServer/src/database/DbManager.java
+++ CineCollectionsServer/src/database/DbManager.java
@@ -6,51 +6,63 @@ import java.sql.*;
 import java.util.Properties;
 
 public class DbManager {
-    private Connection _connection;
+    private Connection _serverConnection;
+    private Connection _dbConnection;
+    Properties _credentials = new Properties();
 
     public DbManager() {
-
+        _credentials.setProperty("user", "postgres");
+        _credentials.setProperty("password", "admin");
+        _credentials.setProperty("protocolVersion", "3");
     }
 
-    private void query(String sql) throws DbException {
+    private Connection initConnection(String suffix) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + suffix, _credentials);
+        connection.setAutoCommit(true);
+        return connection;
+    }
+
+    private void query(String sql, Connection connection) throws DbException {
         try {
-            _connection.createStatement().executeUpdate(sql);
+            connection.createStatement().executeUpdate(sql);
         } catch (SQLException throwables) {
             throw new DbException("Failed to create database: " + throwables.getMessage());
         }
     }
 
-    public void connect() throws DbException { //TODO - Make connection url configurable
-        String url = "jdbc:postgresql://localhost:5432/";
-        Properties p = new Properties();
-        p.setProperty("user", "postgres");
-        p.setProperty("password", "admin");
-        p.setProperty("protocolVersion", "3");
-
+    public void connect() throws DbException { // TODO - Make connection url configurable
         try {
-            _connection = DriverManager.getConnection(url, p);
-            _connection.setAutoCommit(true);
+            _serverConnection = initConnection("");
+            System.out.println("Connected to SQL Server at localhost:5432");
+            initializeDatabase();
         } catch (SQLException throwables) {
             throw new DbException("Failed to connect to database: " + throwables.getMessage(), throwables);
         }
-        System.out.println("Connected to SQL Server at localhost:5432");
-        initialize();
     }
 
-    private void initialize() throws DbException {
-        if (!dbExists("userdb")) {
-            System.out.println("Initializing User Database");
-            query("CREATE DATABASE userdb;");
+    private void initializeDatabase() throws DbException, SQLException {
+        if (!dbExists("cinecollectionsdb")) {
+            System.out.println("Initializing CineCollections Database");
+            query("CREATE DATABASE cinecollectionsdb;", _serverConnection);
         }
-        if (!dbExists("collectionsdb")) {
-            System.out.println("Initializing Collection Database");
-            query("CREATE DATABASE collectionsdb;");
-        }
+
+        _dbConnection = initConnection("cinecollectionsdb");
+        query("CREATE TABLE IF NOT EXISTS users(" +
+                    "id SERIAL PRIMARY KEY," +
+                    "username VARCHAR (256)," +
+                    "password VARCHAR (256));", _dbConnection); // TODO - Hash passwords
+        query("CREATE TABLE IF NOT EXISTS collections(" +
+                    "collection_id SERIAL PRIMARY KEY," +
+                    "user_id INTEGER REFERENCES users(id)," +
+                    "collection_name VARCHAR (256)," +
+                    "films VARCHAR (256));", _dbConnection);
+
+        System.out.println("Database Initialized");
     }
 
     private boolean dbExists(String dbname) throws DbException {
         try {
-            Statement statement = _connection.createStatement();
+            Statement statement = _serverConnection.createStatement();
             String sql = "SELECT EXISTS (SELECT datname FROM pg_catalog.pg_database WHERE datname = '" + dbname + "');";
             ResultSet resultSet = statement.executeQuery(sql);
             if (resultSet.next()) {
@@ -66,9 +78,8 @@ public class DbManager {
 
     public void disconnect() throws DbException {
         try {
-            if (!_connection.isClosed()) {
-                _connection.close();
-            }
+            _dbConnection.close();
+            _serverConnection.close();
         } catch (SQLException throwables) {
             throw new DbException("Failed to disconnect from database: " + throwables.getMessage(), throwables);
         }
