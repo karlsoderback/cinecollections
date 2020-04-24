e1a6ccf9ad9aa3ff42fa16f47482ec6156836875
Karl Söderbäck <karso527@student.liu.se>
2020-04-24 14:46:01 +0200

    NEW: Set up functionality for creating a new user through the REST API into the database

--- CineCollectionsServer/src/database/DbManager.java
+++ CineCollectionsServer/src/database/DbManager.java
@@ -1,7 +1,9 @@
 package database;
 
 import exception.DbException;
+import org.json.JSONObject;
 
+import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.Properties;
 
@@ -22,11 +24,19 @@ public class DbManager {
         return connection;
     }
 
-    private void query(String sql, Connection connection) throws DbException {
+    private void executeUpdate(String sql, Connection connection) throws DbException {
         try {
             connection.createStatement().executeUpdate(sql);
         } catch (SQLException throwables) {
-            throw new DbException("Failed to create database: " + throwables.getMessage());
+            throw new DbException("Failed to execute update: " + throwables.getMessage(), throwables);
+        }
+    }
+
+    private ResultSet executeQuery(String sql, Connection connection) throws DbException {
+        try {
+            return connection.createStatement().executeQuery(sql);
+        } catch (SQLException throwables) {
+            throw new DbException("Failed to execute query: " + throwables.getMessage(), throwables);
         }
     }
 
@@ -43,15 +53,15 @@ public class DbManager {
     private void initializeDatabase() throws DbException, SQLException {
         if (!dbExists("cinecollectionsdb")) {
             System.out.println("Initializing CineCollections Database");
-            query("CREATE DATABASE cinecollectionsdb;", _serverConnection);
+            executeUpdate("CREATE DATABASE cinecollectionsdb;", _serverConnection);
         }
 
         _dbConnection = initConnection("cinecollectionsdb");
-        query("CREATE TABLE IF NOT EXISTS users(" +
+        executeUpdate("CREATE TABLE IF NOT EXISTS users(" +
                     "id SERIAL PRIMARY KEY," +
-                    "username VARCHAR (256)," +
+                    "username VARCHAR (256) UNIQUE," +
                     "password VARCHAR (256));", _dbConnection); // TODO - Hash passwords
-        query("CREATE TABLE IF NOT EXISTS collections(" +
+        executeUpdate("CREATE TABLE IF NOT EXISTS collections(" +
                     "collection_id SERIAL PRIMARY KEY," +
                     "user_id INTEGER REFERENCES users(id)," +
                     "collection_name VARCHAR (256)," +
@@ -83,5 +93,10 @@ public class DbManager {
         } catch (SQLException throwables) {
             throw new DbException("Failed to disconnect from database: " + throwables.getMessage(), throwables);
         }
+    }
+
+    public void createNewUser(String username, String password) throws DbException {
+        executeUpdate("INSERT INTO users(username, password) VALUES (\'" + username + "\', \'" + password + "\');", _dbConnection);
+        System.out.println("User \"" + username + " added to database");
     }
 }
