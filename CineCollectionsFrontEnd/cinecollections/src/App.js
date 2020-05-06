63d967c92bc5a764a023ece1f283c3d5d35c0513
Karl Söderbäck <karso527@student.liu.se>
2020-05-06 08:28:02 +0200

    NEW: Created react app

---
+++ CineCollectionsFrontEnd/cinecollections/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+    class App extends Component {
+      render () {
+        return (
+          "Hello world"
+          // JSX to render goes here...
+          );
+      }
+    }
+
+export default App;
