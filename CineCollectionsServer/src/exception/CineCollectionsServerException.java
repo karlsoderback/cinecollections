d7597334285b8fd41562701ddef9080594244f3f
Karl Söderbäck <karso527@student.liu.se>
2020-05-07 11:14:18 +0200

    REF: Renamed backend package to CineCollectionsBackend

--- CineCollectionsServer/src/exception/CineCollectionsServerException.java
+++
@@ -1,6 +0,0 @@
-package exception;
-
-public class CineCollectionsServerException extends Exception {
-    public CineCollectionsServerException(String message) { super(message); }
-    public CineCollectionsServerException(String message, Throwable cause) { super(message, cause); }
-}
