8f8f231e9bbe7c3222c5178e7e40c3fea09fe2e7
Karl Söderbäck <karso527@student.liu.se>
2020-05-22 10:15:26 +0200

    REF: Refactored profile to be a class

--- CineCollectionsFrontEnd/cinecollections/src/pages/profile.js
+++ CineCollectionsFrontEnd/cinecollections/src/pages/profile.js
@@ -1,38 +1,49 @@
 import React, { useState } from "react";
 
 import { browserHistory } from "react-router";
-import { connect } from "react-redux";
+import { connect, ReactReduxContext } from "react-redux";
 import { logged_out } from "../redux/actions.js";
 
 import { sendGET } from "../rest/rest.js"
 
 import collection from "../models/collection.js";
 
-function Profile(props) {
-    const [myCollections, setMyCollections] = useState("");
-    const [subscribedCollections, setSubscribedCollections] = useState("");
+class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            myCollections: [],
+            subscribedCollections: []
+        };
+
+        this.logOut = this.logOut.bind(this);
+        
+    }
     
-    /*sendGET("collection/getallforuser?username=" + props.username).then(
-        data => {
-            setMyCollections(data.my_collections);
-            setSubscribedCollections(data.subscribed_collections);
-            console.log("My collections: ");
-            console.log(myCollections);
-            console.log("Subcribed collections: ");
-            console.log(subscribedCollections);
-        }
-    )*/ //TODO - investigate spam bug
-
-    function logOut() {
-        props.dispatch(logged_out());
-        browserHistory.push("/");
-    }
-    function home(){
-        browserHistory.push("/");
+    componentDidMount() {
+        sendGET("collection/getallforuser?username=" + this.props.username).then(
+            data => {
+                this.setState({myCollections: data.my_collections});
+                this.setState({subscribedCollections: data.subscribed_collections});
+                console.log("My collections: ");
+                console.log(this.state.myCollections);
+                console.log("Subcribed collections: ");
+                console.log(this.state.subscribedCollections);
+            }
+        )
     }
 
-    function renderCollections(){    
-        for (var i = 0; i < myCollections.length; i++) {
+
+    logOut() {
+        this.props.dispatch(logged_out());
+        browserHistory.push("/");
+    }
+    home(){
+        browserHistory.push("/");
+    }
+
+    renderCollections(){    
+        for (var i = 0; i < this.state.myCollections.length; i++) {
             // TODO - render all collections here
         }
 
@@ -43,17 +54,19 @@ function Profile(props) {
         )
     }
 
-    return (
-        <div className="profile">
-            <h1>{props.username}</h1>
-            <button onClick={logOut}>Log out</button>
-            <button onClick={home}>Home</button>
-            <div className="collections">
-                <h2>Collections</h2>
-
+    render() {
+        return (
+            <div className="profile">
+                <h1>{this.props.username}</h1>
+                <button onClick={this.logOut}>Log out</button>
+                <button onClick={this.home}>Home</button>
+                <div className="collections">
+                    <h2>Collections</h2>
+    
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 function mapStateToProps(state) {
